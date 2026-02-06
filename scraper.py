f737f40e8dcc94ed62cd10487e62016cbfe998f9
Achyutashivam <shivam@achyutagroup.com>
2026-02-06 10:56:58 +0530

    dhg

--- scraper.py
+++ scraper.py
@@ -7,7 +7,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
-from selenium.common.exceptions import InvalidSessionIdException, WebDriverEx
+from selenium.common.exceptions import InvalidSessionIdException
 import time
 import json
 import re
